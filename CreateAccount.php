a4f35dc597df5ae5264b532c0945fd5cd7dfc9f8
willwill96 <wbw4sv@virginia.edu>
2016-10-08 14:34:50 -0400

    Added Ability to Create Account
    added php file that sends sql query to database

---
+++ CreateAccount.php
@@ -0,0 +1,40 @@
+<?php
+
+$user_name = "root";
+$password = NULL;
+$database = "rpgcharity";
+$server = "localhost";
+
+mysql_connect("$server","$user_name","$password");
+
+mysql_select_db("$database");
+$first=$_POST['first'];
+$last=$_POST['last'];
+$email=$_POST['email'];
+$address=$_POST['address'];
+$city=$_POST['city'];
+$state=$_POST['state'];
+$zip=$_POST['zipcode'];
+
+
+$order = "INSERT INTO siteusers
+
+        (first_name, last_name, email, address, city, state, zip_code)
+
+        VALUES
+
+        ('$first', '$last', '$email', '$address', '$city', '$state', '$zip')";
+
+
+$result = mysql_query($order);
+
+if($result){
+
+    echo("<br>Input data is succeed");
+
+} else{
+
+    echo("<br>Input data is fail");
+
+}
+?>
